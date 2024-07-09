7e0753419890143faebeb947f74486e43eaac1b3
BuildTools <unconfigured@null.spigotmc.org>
2024-07-10 02:58:15 +0900

    test: cicd 3

--- src/main/java/store/itpick/backend/BackendApplication.java
+++ src/main/java/store/itpick/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-	// cicd test 2
+	// cicd test 3
 
 
 	public static void main(String[] args) {
