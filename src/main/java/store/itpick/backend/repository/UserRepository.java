c5dcee2662325973b97f190102a3852b9224e14b
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-07-18 15:03:04 +0900

    Feat: UserRepository

---
+++ src/main/java/store/itpick/backend/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package store.itpick.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import store.itpick.backend.model.User;
+public interface UserRepository extends JpaRepository<User,Long> {
+}
