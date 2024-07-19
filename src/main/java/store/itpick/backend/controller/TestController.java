aa0ba8599491a38f288ce3e3f31052d01b25cf29
Minseok <saltishDev@gmail.com>
2024-07-19 21:24:11 +0900

    Feat: 셀레니움 도입

--- src/main/java/store/itpick/backend/controller/TestController.java
+++ src/main/java/store/itpick/backend/controller/TestController.java
@@ -1,9 +1,16 @@
 package store.itpick.backend.controller;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import store.itpick.backend.util.Selenium;
+
+import java.io.IOException;
 
 @RestController
 public class TestController {
@@ -17,5 +24,40 @@ public class TestController {
         redisTemplate.opsForValue().set("testKey", "Hello, Redis!");
         String value = redisTemplate.opsForValue().get("testKey");
         return value;
+    }
+
+    @GetMapping("/test20")
+    void testJsoup20() throws IOException {
+        String homeUrl = "https://news.sbs.co.kr/news/newsMain.do";
+        Document doc = Jsoup.connect(homeUrl).get();
+        Elements elements = doc.select("li > a > p > strong");
+        System.out.println(elements);
+    }
+
+    @GetMapping("/test21")
+    void testJsoup21() throws IOException {
+        String homeUrl = "https://trends.google.com/trends/trendingsearches/daily?geo=KR&hl=ko";
+        Connection.Response response = Jsoup.connect(homeUrl).method(Connection.Method.GET).execute();
+        Document doc = response.parse();
+        System.out.println(doc.html());
+    }
+
+    @GetMapping("/test22")
+    public String test() {
+
+        //JsoupFun um = new JsoupFun();
+
+        Selenium sel = new Selenium();
+
+        String url = "https://trends.google.com/trends/trendingsearches/daily?geo=KR&hl=ko";
+
+        sel.useDriver(url);
+
+        return "main";
+    }
+
+    @GetMapping("/*")
+    void pathMatch() {
+        System.out.println("favicon.ico.");
     }
 }
