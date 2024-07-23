f73e083d0a9be6b1255835bd5c3fc92d16aff1d7
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-07-23 19:15:34 +0900

    Delete: userDao

--- src/main/java/store/itpick/backend/service/UserService.java
+++ src/main/java/store/itpick/backend/service/UserService.java
@@ -6,20 +6,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import store.itpick.backend.common.exception.UserException;
 import store.itpick.backend.common.response.status.BaseExceptionResponseStatus;
-import store.itpick.backend.dao.UserDao;
 import store.itpick.backend.dto.user.user.PostUserRequest;
 import store.itpick.backend.dto.user.user.PostUserResponse;
 import store.itpick.backend.model.User;
+import store.itpick.backend.repository.UserRepository;
 
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserDao userDao;
+    private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Transactional
@@ -37,7 +38,7 @@ public class UserService {
         // Create user
         User user = User.builder().email(postUserRequest.getEmail()).password(encodedPassword).nickname(postUserRequest.getNickname()).birth_date(postUserRequest.getBirth_date()).status("active").build();
 
-        user = userDao.save(user);
+        user = userRepository.save(user);
 
         // 회원가입에서 jwt 생성 시 여기에 로직 포함시키기
         String jwt = "";
@@ -46,18 +47,26 @@ public class UserService {
     }
 
     private void validateEmail(String email) {
-        if (userDao.existsByEmailAndStatusIn(email, List.of("active", "dormant"))) {
+        if (userRepository.existsByEmailAndStatusIn(email, List.of("active", "dormant"))) {
             throw new UserException(BaseExceptionResponseStatus.DUPLICATE_EMAIL);
         }
     }
 
     private void validateNickname(String nickname) {
-        if (userDao.existsByNicknameAndStatusIn(nickname, List.of("active", "dormant"))) {
+        if (userRepository.existsByNicknameAndStatusIn(nickname, List.of("active", "dormant"))) {
             throw new UserException(BaseExceptionResponseStatus.DUPLICATE_NICKNAME);
         }
     }
 
     public void modifyUserStatus_deleted(long userId) {
-        userDao.modifyUserStatus_deleted(userId);
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setStatus("deleted");
+            userRepository.save(user);
+        } else {
+            throw new UserException(BaseExceptionResponseStatus.USER_NOT_FOUND);
+        }
     }
+
 }
