982ff724af27876504d1ee966ccb96d1ebb6dc6a
김민석 <skyfox001015@naver.com>
2024-08-05 17:01:33 +0900

    feat : 실시간 검색할 시, 중복된 키워드 저장하면 덮어쓰게 저장하도록 함(reference는 현재 꺼로 업데이트)

--- src/main/java/store/itpick/backend/repository/KeywordRepository.java
+++ src/main/java/store/itpick/backend/repository/KeywordRepository.java
@@ -10,4 +10,8 @@ public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
     /** Key랑 RedisId를 통해서 관련자료 Id를 조회하는데, 2개 이상의 Id가 조회되는 경우 가장 최신의 것을 선택 **/
     Optional<Keyword> findFirstByRedisIdAndKeywordOrderByCreateAtDesc(String redisId, String keyword);
+
+    /** Keyword저장할 때, 중복된 Keyword+RedisID 조합이 있는지 확인할 때 사용  **/
+    Optional<Keyword> findFirstByRedisIdAndKeyword(String redisId, String keyword);
+
 }
--- src/main/java/store/itpick/backend/service/KeywordService.java
+++ src/main/java/store/itpick/backend/service/KeywordService.java
@@ -6,7 +6,10 @@ import org.springframework.transaction.annotation.Transactional;
 import store.itpick.backend.model.Keyword;
 import store.itpick.backend.repository.KeywordRepository;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +17,25 @@ public class KeywordService {
 
     private final KeywordRepository keywordRepository;
 
-    //keyword 저장함
     @Transactional
     public void saveAll(List<Keyword> keywords) {
-        keywordRepository.saveAll(keywords);
+        for (Keyword keyword : keywords) {
+            Optional<Keyword> existingKeyword = keywordRepository.findFirstByRedisIdAndKeyword(keyword.getRedisId(), keyword.getKeyword());
+
+            if (existingKeyword.isPresent()) {
+                // 기존의 엔티티가 존재하면, 해당 엔티티의 필드 값을 업데이트
+                Keyword keywordToUpdate = existingKeyword.get();
+                keywordToUpdate.setStatus(keyword.getStatus());
+                keywordToUpdate.setReference(keyword.getReference());
+                keywordToUpdate.setUpdateAt(Timestamp.from(Instant.now())); // 업데이트 시간 변경
+                // 필요한 경우 다른 필드도 업데이트
+
+                keywordRepository.save(keywordToUpdate);
+            } else {
+                // 존재하지 않으면 새로운 엔티티를 저장
+                keywordRepository.save(keyword);
+            }
+        }
     }
 
     @Transactional
