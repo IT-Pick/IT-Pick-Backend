daf44ee1b9139cf0fd04da574d44aa0ef6dfc272
Minseok <saltishDev@gmail.com>
2024-08-06 19:59:41 +0900

    Fix: @EnableRedisRepositories 어노테이션 추가

--- src/main/java/store/itpick/backend/config/RedisConfig.java
+++ src/main/java/store/itpick/backend/config/RedisConfig.java
@@ -2,13 +2,12 @@ package store.itpick.backend.config;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableRedisRepositories
